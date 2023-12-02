5b53f1aa1c465fec39045fabe4f473663b1b70c4
Nada Abbas <N.Elsayed@nu.edu.eg>
2023-12-02 22:06:55 +0100

    steer_to_avoid second_d

--- src/utils/steer_to_avoid.py
+++ src/utils/steer_to_avoid.py
@@ -7,11 +7,13 @@ from ompl import geometric as og
 class Steer_to_avoid:
 
     # Constructor
-    def __init__(self):
+    def __init__(self,obstacle_radius,step_angle,max_steering_angle):
         # map: 2D array of integers which categorizes world occupancy
         self.map = None 
         self.map_dim = None
-
+        self.step_angle = step_angle
+        self.max_steering_angle = max_steering_angle
+        self.obstacle_radius=obstacle_radius
         # map sampling resolution (size of a cell))                            
         self.resolution = None
         
@@ -31,12 +33,13 @@ class Steer_to_avoid:
         self.origin = np.array(origin)
         self.there_is_map = True
     
-    def get_steering_direction(self, grid_map, boid, max_steering_angle, neighborhood_radius, step_angle):
-        boid_x, boid_y, boid_theta = boid
+    def get_steering_direction(self, boid):
+        boid_x, boid_y = boid
+        boid_theta = math.atan2(boid_y,boid_x)
         steering_adjustment = 0
 
-        lookahead_x = boid_x + neighborhood_radius * math.cos(math.radians(boid_theta))
-        lookahead_y = boid_y + neighborhood_radius * math.sin(math.radians(boid_theta))
+        lookahead_x = boid_x + self.obstacle_radius * math.cos(boid_theta)
+        lookahead_y = boid_y + self.obstacle_radius * math.sin(boid_theta)
 
         map_coords = self._position_to_map([lookahead_x, lookahead_y])
         if not map_coords:
@@ -44,41 +47,39 @@ class Steer_to_avoid:
 
         grid_x, grid_y = int(map_coords[0]), int(map_coords[1])
 
-        while grid_map[grid_y][grid_x] == 100 and abs(steering_adjustment) <= max_steering_angle:
-            steering_adjustment += step_angle
+        while self.map[grid_y][grid_x] == 100 and abs(steering_adjustment) <= self.max_steering_angle:
+            steering_adjustment += self.step_angle
             new_theta = boid_theta + steering_adjustment
 
-            new_grid_x = int(boid_x + neighborhood_radius * math.cos(math.radians(new_theta)))
-            new_grid_y = int(boid_y + neighborhood_radius * math.sin(math.radians(new_theta)))
+            new_grid_x = int(boid_x + self.obstacle_radius * math.cos(new_theta))
+            new_grid_y = int(boid_y + self.obstacle_radius * math.sin(new_theta))
 
-            if grid_map[new_grid_y][new_grid_x] != 100:
+            if self.map[new_grid_y][new_grid_x] != 100:
                 boid_theta = new_theta
                 break
 
             new_theta = boid_theta - steering_adjustment
 
-            new_grid_x = int(boid_x + neighborhood_radius * math.cos(math.radians(new_theta)))
-            new_grid_y = int(boid_y + neighborhood_radius * math.sin(math.radians(new_theta)))
+            new_grid_x = int(boid_x + self.obstacle_radius * math.cos(new_theta))
+            new_grid_y = int(boid_y + self.obstacle_radius * math.sin(new_theta))
 
-            if grid_map[new_grid_y][new_grid_x] != 100:
+            if self.map[new_grid_y][new_grid_x] != 100:
                 boid_theta = new_theta
                 break
 
         return boid_theta
     
-    def compute(self,map,boid,local_r,step_angle,max_steering_angle): 
-
-        steering_angle = self.get_steering_direction(map,boid,max_steering_angle,local_r,step_angle)
+    def compute(self,map,boid): 
+        steering_angle = self.get_steering_direction(map,boid)
         self.steering_force = self._create_steering_force(steering_angle)
+        return self.steering_force
 
 
     def _create_steering_force(steering_angle):
         fx= math.cos(steering_angle)
         fy= math.sin(steering_angle)
-        steering_force=(fx,fy)
-        return(steering_force)
-
-
+        steering_force=[fx,fy]
+        return steering_force
 
     def _position_to_map(self, p):
         # TODO: convert world position to map coordinates. If position outside map return `[]` or `None`
