53463e62c9a8d85a00458d74428137df6aa9d33e
MosesEbere <moseschukaebere@gmail.com>
2023-12-03 02:23:18 +0100

    Navigation Updates

--- src/reynolds.py
+++ src/reynolds.py
@@ -5,6 +5,9 @@ from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 
+from scipy.spatial import ConvexHull
+import matplotlib.pyplot as plt
+
 import numpy as np
 
 from utils.state_tf  import pose_msg_to_state
@@ -19,7 +22,28 @@ class Reynolds:
                         rospy.get_param('~w_c'),
                         rospy.get_param('~w_s')]
         self.percep_field = [rospy.get_param('~local_r'),
-                              rospy.get_param('~local_theta')]
+                              rospy.get_param('~local_theta')]        
+        self.goal = [rospy.get_param('~goal_x'),
+                     rospy.get_param('~goal_y'),
+                     rospy.get_param('~goal_radius'),
+                     rospy.get_param('~goal_tolerance')]
+        
+        self.goal_list = rospy.get_param('~goal_list')
+
+        self.max_speed = rospy.get_param('~max_speed')
+        self.max_acc = rospy.get_param('~max_acc')
+        self.n_leader = rospy.get_param('~n_leader')
+        self.leader_method = rospy.get_param('~leader_method')
+        self.inter_goal_dist = rospy.get_param('~inter_goal_dist')
+        self.use_prioritized_acc = rospy.get_param('~use_prioritized_acc')
+        self.priority_list = rospy.get_param('~priority_list')
+        self.kwargs = {'goal': self.goal, 'max_speed': self.max_speed, 
+                       'max_acc': self.max_acc, 'use_prioritized_acc': self.use_prioritized_acc,
+                       'priority_list': self.priority_list}
+
+        self.compute_goals = True if self.n_leader > 0 else False
+        # All boids have been created
+        self.all_boids_created = False
 
         rospy.loginfo('n_boids: %s', self.n_boids)
         rospy.loginfo('weights [w_a, w_c, w_s]: %s', self.weights)
@@ -38,13 +62,7 @@ class Reynolds:
 
             pub = rospy.Publisher('/robot_{}/cmd_vel'.format(i), Twist, queue_size=1)
             self.pubs.append(pub)
-
-        # # Create publisher to n robots' cmd_vel dynamically Publishers /robot_{i}/cmd_vel
-        
-        for i in range(self.n_boids):
-            topic_name = '/robot_{}/cmd_vel'.format(i)
             
-
         # execute algorithm 
         rospy.Timer(rospy.Duration(0.1), self.run)
 
@@ -65,7 +83,100 @@ class Reynolds:
             self.boids[robot_id].set_state(x)
         else:
             # Create boid instance
-            self.boids[robot_id] = Boid(robot_id,self.weights,self.percep_field,x)
+            self.boids[robot_id] = Boid(robot_id, self.weights, self.percep_field, x, **self.kwargs)
+            # Set a flag when all boids have been created
+            if None not in self.boids:
+                self.all_boids_created = True
+
+
+    @classmethod
+    def point_angle_length(cls, p1=[], angle=0, length=1):
+        # A segment can be initialized with a Point object, an angle, and a segment length.
+        x2 = p1[0] + np.cos(angle) * length
+        y2 = p1[1] + np.sin(angle) * length
+        return [x2, y2]
+
+
+    def generate_goals(self):
+        positions = np.array([boid.get_pos() for boid in self.boids])
+
+        # Find the boids closest to the goal
+        distances = np.linalg.norm(positions - self.goal[:2], axis=1)
+        closest_boids = np.argsort(distances)[:self.n_leader]
+
+        # # Create the goals using point angle length. Each angle is 60 degrees from the previous angle.
+        # # The length is the boid's local radius.
+        goals = [self.goal]
+        # Convert 60 degrees to radians
+        angle = np.pi / 3
+        for i in range(self.n_leader - 1):
+            goals.append([*self.point_angle_length(self.goal[:2], angle, self.percep_field[0] * self.inter_goal_dist), self.goal[2], self.goal[3]])
+            angle += np.pi / 3
+
+        # Find the closest boid to each goal
+        for goal in goals:
+            distances = np.linalg.norm(positions[closest_boids] - goal[:2], axis=1)
+            closest_boid = closest_boids[np.argmin(distances)]
+            # Delete the boid from the closest boids so that it is not assigned to another goal
+            closest_boids = np.delete(closest_boids, np.argmin(distances))
+            self.boids[closest_boid].set_goal(goal)
+
+        # Plot the goals assigned to each boid
+        for boid in self.boids:
+            if boid == None:
+                continue
+            if boid.get_goal() == None:
+                continue
+            plt.plot(boid.get_goal()[0], boid.get_goal()[1], 'ro')
+        plt.show()
+
+
+    def convex_hull(self):
+        positions = np.array([boid.get_pos() for boid in self.boids])
+
+        # Find the boids that are the 4 corners of the convex hull
+        hull = ConvexHull(positions)
+        hull_vertices = hull.vertices
+        hull_points = positions[hull_vertices]
+        hull_points = np.append(hull_points, hull_points[0]).reshape(-1, 2)
+
+        # Find the index of each of the hull points in the positions array
+        hull_indices = []
+        for hull_point in hull_points:
+            hull_indices.append(np.where((positions == hull_point).all(axis=1))[0][0])
+
+        # Draw a line between each of the hull points using matplotlib
+        for i in range(len(hull_points) - 1):
+            plt.plot([hull_points[i][0], hull_points[i+1][0]], [hull_points[i][1], hull_points[i+1][1]], 'k-')
+
+        # Create the goals using point angle length. Each angle is 60 degrees from the previous angle.
+        # The length is the boid's local radius.
+        goals = [self.goal]
+        # Convert 60 degrees to radians
+        angle = np.pi / 3
+        for i in range(self.n_leader - 1):
+            goals.append([*self.point_angle_length(self.goal[:2], angle, self.percep_field[0] * self.inter_goal_dist), self.goal[2], self.goal[3]])
+            angle += np.pi / 3
+
+        # Find the closest boid to each goal
+        for goal in goals:
+            distances = np.linalg.norm(hull_points - goal[:2], axis=1)
+            closest_boid = np.argmin(distances)
+            index = hull_indices[closest_boid]
+            # Delete the boid from the hull indices so that it is not assigned to another goal
+            del hull_indices[closest_boid]
+            # Delete the boid from the hull points so that it is not assigned to another goal
+            hull_points = np.delete(hull_points, closest_boid, axis=0)
+            self.boids[index].set_goal(goal)
+
+        # Plot the goals assigned to each boid
+        for boid in self.boids:
+            if boid == None:
+                continue
+            if boid.get_goal() == None:
+                continue
+            plt.plot(boid.get_goal()[0], boid.get_goal()[1], 'ro')
+        plt.show()
 
 
     def find_neighbors(self, boid):
@@ -75,16 +186,13 @@ class Reynolds:
             # Skip if boid is self or if boid has not been created yet
             if self.boids[i] == None:
                 continue
-            if self.boids[i] != boid :
-                dx = self.boids[i].get_pos()[0] - boid.get_pos()[0]
-                dy = self.boids[i].get_pos()[1] - boid.get_pos()[1]
+            if self.boids[i] != boid:
                 angle_diff = abs(self.boids[i].get_theta() - boid.get_theta())
                 angle_diff = (angle_diff + np.pi) % (2 * np.pi) - np.pi
                 if abs(angle_diff) < boid.local_theta / 2:
                     distance = np.linalg.norm(np.array(boid.get_pos()) - np.array(self.boids[i].get_pos()))
                     if distance <= boid.local_r:
                         neighbors.append(self.boids[i])
-        print("here",neighbors)
         return neighbors
     
     # Write a function that takes in a boid and cmd_vel and publishes it to the appropriate topic
@@ -97,10 +205,17 @@ class Reynolds:
         self.pubs[boid.id].publish(cmd_vel)
 
 
-    def run(self,event):
-        print('-------------------------')
-        # TODO: Change the control of the number of boids from the simulator.
-        print(self.boids)
+    def run(self, event):
+        # Generate goals (if necessary) for leader boids.
+        if self.compute_goals and self.all_boids_created:
+            if self.leader_method == 0:
+                self.generate_goals()
+            elif self.leader_method == 1:
+                self.convex_hull()
+            self.compute_goals = False
+
+        if self.all_boids_created == False:
+            return 
         # Ensure that all boids have been created and have a position and velocity before running the algorithm
         for b in self.boids:
             if b == None:
@@ -108,13 +223,14 @@ class Reynolds:
             if b.get_pos() == None:
                 continue
 
-            print(f"Boid {b.id} is at loc: {b.get_pos()} with velocty: {b.get_linvel()}")
             neighbors = self.find_neighbors(b)
-
             b.set_neighbors(neighbors)
             cmd_vel = b.update(self.boids)
 
-            # TODO: publish the cmd velocity to the appropriate boids topic
+            #TODO: check if the goal has been reached, then set compute goal to get a new goal
+            #based on the list of goals provided
+
+            # publish the cmd velocity to the appropriate boids topic
             self.publish_cmd_vel(b, cmd_vel)
         
 
--- src/utils/Boid.py
+++ src/utils/Boid.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Boid:
 
-    def __init__(self,id,weights,percep_field,state):
+    def __init__(self,id,weights,percep_field,state, **kwargs):
         
         self.id = id
         self.w_a, self.w_c, self.w_s = weights
@@ -13,6 +13,19 @@ class Boid:
 
         # list of neigbors as boid objects
         self.neighbors = None
+
+        # Desired goal - [x, y, radius, tolerance]
+        self.goal = None
+
+        # Max speed
+        self.max_speed = kwargs['max_speed']
+        # Max acceleration
+        self.max_acc = kwargs['max_acc']
+
+        # Use prioritized acceleration
+        self.use_prioritized_acc = kwargs['use_prioritized_acc']
+        self.priority_list = kwargs['priority_list']
+
 
     def get_pos(self):
         '''
@@ -50,6 +63,12 @@ class Boid:
         '''
         return self.state[3:]
     
+    def get_goal(self):
+        '''
+        Returns goal as [x,y,radius,tolerance]
+        '''
+        return self.goal
+    
     def set_state(self,state):
         '''
         Updates state of boid
@@ -61,10 +80,24 @@ class Boid:
         Updates the neighbors of the boid
         '''
         self.neighbors = neighbor
-            
-    def update(self,boids=None):
+
+    def set_goal(self, goal):
         '''
-        Updates the robot pose and neighborhood.
+        Updates the goal of the boid
+        '''
+        self.goal = goal
+
+    def behavior_function(self, behavior):
+        '''
+        Dynamically calls the behavior function
+        '''
+        name = list(behavior.keys())[0]
+        weight = list(behavior.values())[0]
+        return weight * getattr(self, name)()
+            
+    def update(self, boids=None):
+        '''
+        Calls and combines behaviors.
 
         Parameters:
         boids: list of all boids as Boid instances
@@ -72,58 +105,96 @@ class Boid:
         Returns:
         - cmd_vel: desired velocities of the boid as [v_x,v_y]
         '''
-        
-        # TODO: update neighborhood 
-
         # Compute acceleration
         acc = self._combine_behaviours()
 
-        # TODO: Compute desired velocities from desired accel
-        cmd_vel = self.get_linvel() + acc
+        # Compute velocity
+        if self.get_goal() != None:
+            cmd_vel = np.array([0., 0.]) if acc[0] == 0.0 and acc[1] == 0.0 else np.tanh(acc) # **
+        else:
+            cmd_vel = np.array(self.get_linvel()) + acc
+
+        # Clip the velocity before returning it. 
+        cmd_vel = np.clip(cmd_vel, -self.max_speed, self.max_speed)
 
         return cmd_vel
+    
+    def _seek(self):
+        ''' Computes required acceleration for the boid to seek the goal.'''
+        acc = np.array([0., 0.])
+        if self.goal != None:
+            distance = np.array(np.array(self.get_pos()) - np.array(self.goal))
+            square_distance = (np.linalg.norm(np.array(self.get_pos()) - np.array(self.goal)))**2
+            # Mitigate zero-division
+            square_distance = 1 if square_distance == 0 else square_distance
+            acc = (distance/square_distance)
+        return acc
 
+    def _move_to_goal(self):
+        ''' Computes required acceleration due to goal. '''
+        desired_velocity = np.array([0., 0.])
+        if self.goal != None:
+            target_offset = np.array(self.goal[:2]) - np.array(self.get_pos())   # goal[x, y], not r or tolerance. 
+            distance = np.linalg.norm(target_offset)
+            if distance < self.goal[3]:
+                print("Distance: ", distance)
+                #TODO: send a flag that goal has been reached and generate a new goal
+                
+                return np.array([0., 0.]) # Set the distance to 0 when it falls below the tolerance. 
+
+            ramped_speed = self.max_speed * (distance / self.goal[2])
+            clipped_speed = np.minimum(ramped_speed, self.max_speed)
+            desired_velocity = (clipped_speed / distance) * target_offset
+        return desired_velocity
 
     def _alignment(self):
         ''' Computes required acceleration due to alignment. '''
-        # TODO
-        acc = [0, 0]
+        acc = np.array([0., 0.])
         if self.neighbors != None and len(self.neighbors) != 0:
             for boid in self.neighbors:
-                acc += np.array(self.get_linvel()) - np.array(boid.get_linvel()) 
+                acc +=  np.array(boid.get_linvel()) - np.array(self.get_linvel()) 
             acc = acc/len(self.neighbors) 
         return acc
-    
 
     def _cohesion(self):
         ''' Computes required acceleration due to cohesion. '''
-        # TODO
-        acc = [0, 0] 
+        acc = np.array([0., 0.]) 
         if self.neighbors != None and len(self.neighbors) != 0:
             for boid in self.neighbors:
-                acc += np.array(self.get_pos()) - np.array(boid.get_pos()) 
+                acc +=  np.array(boid.get_pos()) - np.array(self.get_pos())
             acc = acc/len(self.neighbors) 
         return acc
-    
 
-    def _seperation(self):
+    def _separation(self):
         ''' Computes required acceleration due to seperation. '''
-        # TODO
-        acc = [0, 0]
+        acc = np.array([0., 0.])
         if self.neighbors != None and len(self.neighbors) != 0:
             for boid in self.neighbors:
-                distance = np.array(self.get_pos()) - np.array(boid.get_pos()) 
-                square_distance = (np.linalg.norm(np.array(self.get_pos()) - np.array(boid.get_pos())))**2
+                distance = np.array(boid.get_pos()) - np.array(self.get_pos())
+                square_distance = (np.linalg.norm(np.array(boid.get_pos()) - np.array(self.get_pos())))**2
                 acc += (distance/square_distance)
             acc = acc/len(self.neighbors) 
-        return acc
+        return - acc # Negative because we want to move away from the neighbors.
 
 
     def _combine_behaviours(self):
         ''' Calls behaviours, and computes the net weighted acceleration. '''
-        acc = - self.w_a*self._alignment() - self.w_c*self._cohesion() + self.w_s*self._seperation()
+        if self.get_goal() != None:
+            print(f"This is boid {self.id}")
+            acc = 0.7 * self._move_to_goal() + self.w_a * self._alignment() + self.w_c * self._cohesion() + self.w_s * self._separation()
+        else:
+            acc = np.array([0., 0.])
+            if self.use_prioritized_acc:
+                for behavior in self.priority_list:
+                    acc_request = self.behavior_function(behavior)
+                    acc_mag = np.linalg.norm(acc_request)
+                    if np.linalg.norm(acc) + acc_mag > self.max_acc:
+                        # Clip the acceleration
+                        return np.clip(acc + acc_request, -self.max_acc, self.max_acc)
+                    acc += acc_request
+            else:
+                acc = self.w_a * self._alignment() + self.w_c * self._cohesion() + self.w_s * self._separation()
         return acc
     
 
     # TODO: def _obstacle_avoidance(self):
-    # TODO: def _pursuits(self)
